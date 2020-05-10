7a3bca582a4c5381b80ef3d3ced82902efb38860
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2020-05-10 13:28:30 +0200

    Decoupled receiver code, configuration and program run

---
+++ AKSKubeAuditReceiverSolution/AKSKubeAuditReceiver/ForwarderConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AKSKubeAuditReceiver
+{
+    class ForwarderConfiguration
+    {
+        public string EhubNamespaceConnectionString;
+        public string EventHubName;
+        public string BlobStorageConnectionString;
+        public string BlobContainerName;
+        public string WebSinkURL;
+
+        public void InitConfig()
+        {
+            EhubNamespaceConnectionString = Environment.GetEnvironmentVariable("EhubNamespaceConnectionString");
+            EventHubName = Environment.GetEnvironmentVariable("EventHubName");
+            BlobStorageConnectionString = Environment.GetEnvironmentVariable("BlobStorageConnectionString");
+            BlobContainerName = Environment.GetEnvironmentVariable("BlobContainerName");
+            WebSinkURL = Environment.GetEnvironmentVariable("WebSinkURL");
+
+            EhubNamespaceConnectionString = "Endpoint=sb://kubeauditlogeventhub2.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=u/y6I8F4Y6hyBWhGam6c/3/+l000eEuILcWevcLrss4=";
+            EventHubName = "insights-logs-kube-audit";
+            BlobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=storagekubeauditlog;AccountKey=9h4BHg+RHXq6K/x4GfZrufGWrJR03jHOjeGMeyQH2oBFIJ5TlmaK5DZ2RkYEW6WNhiD1OzaxfssEO8W0//IV5A==;EndpointSuffix=core.windows.net";
+            BlobContainerName = "kubeauditlogcontainer";
+            WebSinkURL = "http://sysdig-agent.sysdig-agent.svc.cluster.local:7765/k8s_audit";
+
+        }
+    }
+}
--- AKSKubeAuditReceiverSolution/AKSKubeAuditReceiver/Program.cs
+++ AKSKubeAuditReceiverSolution/AKSKubeAuditReceiver/Program.cs
@@ -1,125 +1,27 @@
 ﻿using System;
-using System.Text;
 using System.Threading.Tasks;
-using Azure.Storage.Blobs;
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Consumer;
-using Azure.Messaging.EventHubs.Processor;
-using Newtonsoft.Json.Linq;
-using System.Net.Http;
-using System.Collections.Generic;
+using AKSKubeAuditReceiver;
 
-namespace AKSKubeAuditReceiver
+namespace NetCore.Docker
 {
     class Program
     {
 
-        private static readonly HttpClient client = new HttpClient();
-
-        private const string webSinkURL = "http://sysdig-agent.sysdig-agent.svc.cluster.local:7765/k8s_audit";
-
-        static async Task Main()
+        static async Task Main(string[] args)
         {
+            //var counter = 0;
+            //var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            //while (max == -1 || counter < max)
+            //{
+            //    Console.WriteLine($"Counter: {++counter}");
+            //    await Task.Delay(1000);
+            //}
 
-            string ehubNamespaceConnectionString = ReceiverConfiguration.EhubNamespaceConnectionString;
-            string eventHubName = ReceiverConfiguration.EventHubName;
-            string blobStorageConnectionString = ReceiverConfiguration.BlobStorageConnectionString;
-            string blobContainerName = ReceiverConfiguration.BlobContainerName;
-
-            // Read from the default consumer group: $Default
-            string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
-
-            // Create a blob container client that the event processor will use 
-            BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
-
-            // Create an event processor client to process events in the event hub
-            EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, ehubNamespaceConnectionString, eventHubName);
-
-            // Register handlers for processing events and handling errors
-            processor.ProcessEventAsync += ProcessEventHandler;
-            processor.ProcessErrorAsync += ProcessErrorHandler;
-
-            // Start the processing
-            Console.WriteLine("Starting proccessors to read Kubernetes audit events from Event Hubs");
-            await processor.StartProcessingAsync();
-
-            // Wait for 10 seconds for the events to be processed
-            await Task.Delay(TimeSpan.FromSeconds(20));
-
-            // Stop the processing
-            Console.WriteLine("\n - Stopping all processors - \n");
-            await processor.StopProcessingAsync();
+            Console.WriteLine("Starting program");
+            ForwarderRunner runner = new ForwarderRunner();
+            await runner.run();
+            Console.WriteLine("Program ended");
         }
 
-        static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
-        {
-            string randomName = Guid.NewGuid().ToString("n").Substring(0,8);
-            Console.WriteLine("{0} > Recevied event", randomName);
-
-            JObject eventJObj = JObject.Parse(Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
-
-            int i = 0;
-            foreach (JObject record in eventJObj["records"])
-            {
-                //Console.Write("{0}: ", i++);
-                
-                try
-                {
-                    JObject kubeAuditEvent = JObject.Parse(record["properties"]["log"].ToString());
-                    Console.WriteLine("{0} {1} > {2} {3} {4} {5}",
-                        randomName,
-                        i++,
-                        (string)kubeAuditEvent.SelectToken("user.username"),
-                        (string)kubeAuditEvent.SelectToken("verb"),
-                        (string)kubeAuditEvent.SelectToken("objectRef.resource"),
-                        (string)kubeAuditEvent.SelectToken("objectRef.name"));
-
-                    //Console.WriteLine("\n{0} ", kubeAuditEvent.ToString());
-                    //Console.WriteLine("--------------------");
-
-                } catch(Exception e)
-                {
-                    Console.WriteLine("+++++++++++++++++");
-                }
-
-                var values = new Dictionary<string, string>
-                {
-                    { "payload", record["properties"]["log"].ToString() }
-                };
-
-                //sendPost(values);
-
-            }
-
-            //Console.WriteLine("{0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
-            //Console.WriteLine();
-            //Console.WriteLine("============================");
-            //Console.WriteLine();
-
-            // records.properties.log
-
-            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
-            await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
-
-            
-        }
-
-        static async void sendPost(Dictionary<string, string> values)
-        {
-            //https://stackoverflow.com/questions/51134041/post-data-from-a-string-value-to-asp-net-core-web-api-controller-method
-
-            var content = new FormUrlEncodedContent(values);
-            var response = await client.PostAsync(webSinkURL, content);
-            var responseString = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(responseString);
-        }
-
-        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
-        {
-            // Write details about the error to the console window
-            Console.WriteLine($"\tPartition '{ eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
-            Console.WriteLine(eventArgs.Exception.Message);
-            return Task.CompletedTask;
-        }
     }
 }
