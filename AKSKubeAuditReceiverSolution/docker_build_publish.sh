861473d217525e6df7b88d2c6f8258ff3fdc9da3
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2020-05-14 10:17:21 +0200

    Changed name of Docker image

--- AKSKubeAuditReceiverSolution/docker_build_publish.sh
+++ AKSKubeAuditReceiverSolution/docker_build_publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -f Dockerfile . \
-        -t aks-audit-log \
-        -t aks-audit-log:dev \
-        -t sysdiglabs/aks-audit-log \
-        -t sysdiglabs/aks-audit-log:dev &&
-    docker push sysdiglabs/aks-audit-log:dev
+docker build -f AKSKubeAuditReceiver/Dockerfile . \
+        -t aks-audit-log-forwarder \
+        -t aks-audit-log-forwarder:dev \
+        -t sysdiglabs/aks-audit-log-forwarder \
+        -t sysdiglabs/aks-audit-log-forwarder:dev &&
+    docker push sysdiglabs/aks-audit-log-forwarder:dev
